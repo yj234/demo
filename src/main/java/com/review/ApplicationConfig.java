bd159458270cf1e4b599141b5c1b00c1898303e9
yaojian <yj_234@163.com>
2021-03-04 15:38:52 +0800

    注释

--- src/main/java/com/review/ApplicationConfig.java
+++ src/main/java/com/review/ApplicationConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 @MapperScan("com.review.demo.mapper")
 public class ApplicationConfig {
     public ApplicationConfig() {
+        //todo 红色：未加入版权控制  绿色：加入版控制未提交 蓝色：提交有改动 白色：无改动
         System.out.println("ApplicationConfig is running...");
     }
 }
